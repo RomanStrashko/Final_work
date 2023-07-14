12b30546bc8dfe551ac54e0b8329fe3750daaa54
RomanStrashko <113093477+RomanStrashko@users.noreply.github.com>
2023-07-14 07:52:17 +0300

    Add files via upload

---
+++ Сounter.java
@@ -0,0 +1,15 @@
+public class Сounter {
+    private int value;
+
+    public Сounter() {
+        value = 0;
+    }
+
+    public void add() {
+        value++;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
